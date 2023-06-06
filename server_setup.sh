280a5b9207dee7a437088433076d0934013c171d
ThinkFar <31487797+ThinkFar@users.noreply.github.com>
2023-06-07 05:09:18 +0700

    Update and rename baseserversetup.sh to server_setup.sh

--- server_setup.sh
+++ server_setup.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
-apt-get update && apt-get install -y sudo yarn wget curl git vim nano zip unzip tar gzip make python
+apt-get update && apt-get install -y sudo wget curl git vim nano zip unzip tar gzip make python
 curl -sL https://deb.nodesource.com/setup_18.x | bash -
 apt-get install -y nodejs
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add - && echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list && sudo apt update && sudo apt install yarn
 if [ ! -f /usr/src/app/docker-index-placeholder.js ]; then 
   printf 'const http = require("http"); const hostname = "0.0.0.0"; const port = 65321; const server = http.createServer((req, res) => { res.statusCode = 404; res.setHeader("Content-Type", "text/html"); res.end("<!DOCTYPE html><html><head><style>body { background-color: black; color: white; }</style></head><body><h1>404 Not Found</h1></body></html>"); }); server.listen(port, hostname, () => { console.log("Server running at http://"+hostname+":"+port+"/"); });' > /usr/src/app/docker-index-placeholder.js; 
 fi 
