d510c3eedb70dd164d5df44a0e9853b5fe23fb31
ThinkFar <31487797+ThinkFar@users.noreply.github.com>
2023-06-07 01:40:55 +0700

    Update init-server-setup.sh

--- init-server-setup.sh
+++ init-server-setup.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-sudo apt-get update && sudo apt-get install -y wget yarn curl git vim nano zip unzip tar gzip make python sudo nodejs && sudo apt-get update && sudo apt-get upgrade
+apt-get update && apt-get install -y wget yarn curl git vim nano zip unzip tar gzip make python sudo nodejs && sudo apt-get update && sudo apt-get upgrade
 if [ ! -f /usr/src/app/docker-index-placeholder.js ]; then 
   printf 'const http = require("http"); const hostname = "0.0.0.0"; const port = 65321; const server = http.createServer((req, res) => { res.statusCode = 404; res.setHeader("Content-Type", "text/html"); res.end("<!DOCTYPE html><html><head><style>body { background-color: black; color: white; }</style></head><body><h1>404 Not Found</h1></body></html>"); }); server.listen(port, hostname, () => { console.log("Server running at http://"+hostname+":"+port+"/"); });' > /usr/src/app/docker-index-placeholder.js; 
 fi 
