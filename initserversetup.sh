3fc68527bbe9497524e4b5190a3fa9ec321f6ecc
ThinkFar <31487797+ThinkFar@users.noreply.github.com>
2023-06-07 00:31:33 +0700

    Update and rename serversetup.sh to initserversetup.sh

--- initserversetup.sh
+++ initserversetup.sh
@@ -3,4 +3,8 @@ apt-get update && apt-get install -y git yarn curl wget vim nano zip unzip tar g
 if [ ! -f /usr/src/app/docker-index-placeholder.js ]; then 
   printf 'const http = require("http"); const hostname = "0.0.0.0"; const port = 65321; const server = http.createServer((req, res) => { res.statusCode = 404; res.setHeader("Content-Type", "text/html"); res.end("<!DOCTYPE html><html><head><style>body { background-color: black; color: white; }</style></head><body><h1>404 Not Found</h1></body></html>"); }); server.listen(port, hostname, () => { console.log("Server running at http://"+hostname+":"+port+"/"); });' > /usr/src/app/docker-index-placeholder.js; 
 fi 
-node /usr/src/app/docker-index-placeholder.js
+if command -v node >/dev/null; then
+  node /usr/src/app/docker-index-placeholder.js
+else
+  echo "Error: Node.js is not installed or 'node' command is not available."
+fi
